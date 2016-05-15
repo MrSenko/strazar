ad7426702809975f61239ba44059c58d5d4eec9a
Mr. Senko <atodorov@mrsenko.com>
2016-05-15 05:58:42 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+config = {
+    'name' : 'strazar',
+    'version' : '0.2.0',
+    'packages' : find_packages(),
+    'author' : 'Mr. Senko',
+    'author_email' : 'atodorov@mrsenko.com',
+    'license' : 'BSD',
+    'description' : 'Automatic dependency testing',
+    'long_description' : long_description,
+    'url' : 'https://github.com/MrSenko/strazar',
+    'keywords' : ['testing'],
+    'classifiers' : [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    'zip_safe' : False,
+    'install_requires' : ['PyYAML'],
+}
+
+setup(**config)
